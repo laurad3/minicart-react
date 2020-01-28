7232d2f9ac22c893acaafa15a1d90fce08d1a7ef
laurad3 <lauralvicuna@gmail.com>
2020-01-28 21:37:03 +0200

    Updates

---
+++ precache-manifest.a93236b3c817801dde02a6bc3d8e1013.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36b9f7067bd433aae208d59b427c9eb",
+    "url": "/minicart-react/index.html"
+  },
+  {
+    "revision": "35b2db19692668c273dc",
+    "url": "/minicart-react/static/css/2.164b9f60.chunk.css"
+  },
+  {
+    "revision": "fa357a5432e92bac5ca5",
+    "url": "/minicart-react/static/css/main.65fa36d8.chunk.css"
+  },
+  {
+    "revision": "35b2db19692668c273dc",
+    "url": "/minicart-react/static/js/2.63cad1e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/minicart-react/static/js/2.63cad1e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa357a5432e92bac5ca5",
+    "url": "/minicart-react/static/js/main.e80f5c32.chunk.js"
+  },
+  {
+    "revision": "2e7e037be0e4d68dd1e8",
+    "url": "/minicart-react/static/js/runtime-main.952227a4.js"
+  },
+  {
+    "revision": "1204009ee00e88fb846e572b45fb5a11",
+    "url": "/minicart-react/static/media/bag.1204009e.png"
+  },
+  {
+    "revision": "4497cb1b77f87e9687398c3527eca71e",
+    "url": "/minicart-react/static/media/coat.4497cb1b.png"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/minicart-react/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/minicart-react/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/minicart-react/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/minicart-react/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/minicart-react/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/minicart-react/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/minicart-react/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/minicart-react/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/minicart-react/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/minicart-react/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/minicart-react/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/minicart-react/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/minicart-react/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/minicart-react/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/minicart-react/static/media/fa-solid-900.f6121be5.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cead061744bfe884faca4cc863c05e0e.js"
+  "/minicart-react/precache-manifest.a93236b3c817801dde02a6bc3d8e1013.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minicart-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
